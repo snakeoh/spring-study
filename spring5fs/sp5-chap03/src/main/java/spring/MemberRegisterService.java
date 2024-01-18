e449f14284107abdc0b24fe729a3abd19d378526
damon <dh.oh@dtonic.io>
2024-01-18 18:05:11 +0900

    chap 03 - 5 까지

--- spring5fs/sp5-chap03/src/main/java/spring/MemberRegisterService.java
+++ spring5fs/sp5-chap03/src/main/java/spring/MemberRegisterService.java
@@ -12,14 +12,14 @@ public class MemberRegisterService {
 
     public Long regist(RegisterRequest req) {
         // 이메일로 회원 데이터(Member) 조회
-        Member member = memberDao.selectByEmail(req.getEamil());
+        Member member = memberDao.selectByEmail(req.getEmail());
         if (member != null) {
             // 같은 이메일을 가진 회원이 이미 존재하면 익셉션 발생
-            throw new DuplicateMemberException("dup email " + req.getEamil());
+            throw new DuplicateMemberException("dup email " + req.getEmail());
         }
         // 같은 이메일을 가진 회원이 존재하지 않으면 DB 삽입
         Member newMember = new Member(
-            req.getEamil(),
+            req.getEmail(),
             req.getPassword(),
             req.getName(),
             LocalDateTime.now()

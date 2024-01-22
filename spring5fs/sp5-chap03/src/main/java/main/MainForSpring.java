817444015816ac5fb149d1f3fe8dbae6593f5fcb
damon <dh.oh@dtonic.io>
2024-01-22 11:18:28 +0900

    chap 3 - 6.4

--- spring5fs/sp5-chap03/src/main/java/main/MainForSpring.java
+++ spring5fs/sp5-chap03/src/main/java/main/MainForSpring.java
@@ -15,6 +15,7 @@ import spring.MemberListPrinter;
 import spring.MemberNotFoundException;
 import spring.MemberRegisterService;
 import spring.RegisterRequest;
+import spring.VersionPrinter;
 import spring.WrongIdPasswordException;
 
 public class MainForSpring {
@@ -44,7 +45,11 @@ public class MainForSpring {
             } else if (command.startsWith("info")) {
                 processInfoCommand(command.split(" "));
                 continue;
+            } else if (command.equals("version")) {
+                processVersionCommand();
+                continue;
             }
+
             printHelp();
         }
     }
@@ -117,5 +122,10 @@ public class MainForSpring {
         }
         MemberInfoPrinter infoPrinter = ctx.getBean("infoPrinter", MemberInfoPrinter.class);
         infoPrinter.printMemberInfo(arg[1]);
+    }
+
+    private static void processVersionCommand() {
+        VersionPrinter versionPrinter = ctx.getBean("versionPrinter", VersionPrinter.class);
+        versionPrinter.print();
     }
 }

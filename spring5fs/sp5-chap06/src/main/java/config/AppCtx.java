18b70cba0d1a849eee0fbec878564c79207c3462
snakeoh <snakeoh@hanmail.net>
2024-01-25 11:22:22 +0900

    chap 06 - 2.2 빈 객체의 초기화와 소멸 : 커스텀 메서드

--- spring5fs/sp5-chap06/src/main/java/config/AppCtx.java
+++ spring5fs/sp5-chap06/src/main/java/config/AppCtx.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import spring.ChangePasswordService;
 import spring.Client;
+import spring.Client2;
 import spring.MemberDao;
 import spring.MemberInfoPrinter;
 import spring.MemberListPrinter;
@@ -18,6 +19,13 @@ public class AppCtx {
     public Client client() {
         Client client = new Client();
         client.setHost("host");
+        return client;
+    }
+
+    @Bean(initMethod = "connect", destroyMethod = "close")
+    public Client2 client2() {
+        Client2 client = new Client2();
+        client.setHost("hose");
         return client;
     }
 }

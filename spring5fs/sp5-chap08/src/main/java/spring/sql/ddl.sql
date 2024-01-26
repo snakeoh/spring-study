aa4992726ecea61e9a0524d72369ad4fb5aa6731
snakeoh <snakeoh@hanmail.net>
2024-01-26 17:00:35 +0900

    chap 08 - 2.2 DB 테이블 생성, 초기 데이터 입력

---
+++ spring5fs/sp5-chap08/src/main/java/spring/sql/ddl.sql
@@ -0,0 +1,18 @@
+create user 'spring5'@'localhost' identified by 'spring5';
+
+create database spring5fs character set=utf8;
+
+grant all privileges on spring5fs.* to 'spring5'@'localhost';
+
+create table spring5fs.MEMBER (
+    ID int auto_increment primary key,
+    EMAIL varchar(255),
+    PASSWORD varchar(100),
+    NAME varchar(100),
+    REGDATE datetime,
+    unique key (EMAIL)
+) engine=InnoDB character set=utf8;
+
+-- 
+INSERT INTO MEMBER (EMAIL, PASSWORD, NAME, REGDATE)
+VALUES ('mad@mad.net', '1234', 'damon', NOW());

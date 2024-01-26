d372d1ba8f609743c0142744d6f1f6371abb5186
snakeoh <snakeoh@hanmail.net>
2024-01-26 14:29:56 +0900

    chap 07 - 4. 프록시 생성 방식

--- spring5fs/sp5-chap07/src/main/java/config/AppCtx.java
+++ spring5fs/sp5-chap07/src/main/java/config/AppCtx.java
@@ -8,7 +8,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
-@EnableAspectJAutoProxy
+// @EnableAspectJAutoProxy
+// 빈 객체가 인터페이스를 상속할 때 인터페이스가 아니 클래스를 이용해서 프록시를 생성하고 싶다면 아래처럼
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class AppCtx {
 
     @Bean
@@ -16,6 +18,8 @@ public class AppCtx {
         return new ExeTimeAspect();
     }
 
+    // 설정 클래스:
+    // AOP 적용 시 RecCalaulator가 상속받은 Calculator 인터페이스를 이용해서 프로시 생성
     @Bean
     public Calculator calculator() {
         return new RecCalculator();
--- spring5fs/sp5-chap07/src/main/java/main/MainAspect.java
+++ spring5fs/sp5-chap07/src/main/java/main/MainAspect.java
@@ -1,6 +1,7 @@
 package main;
 
 import chap07.Calculator;
+import chap07.RecCalculator;
 import config.AppCtx;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -11,7 +12,10 @@ public class MainAspect {
             AppCtx.class
         );
 
-        Calculator cal = ctx.getBean("calculator", Calculator.class);
+        // Calculator cal = ctx.getBean("calculator", Calculator.class);
+        // "calculator" 빈의 실제 타입은 Calculator를 상속한 프록시 타입이므로
+        // RecCalculator로 타입 변환을 할 수 없기 때문에 익셉션 발생
+        Calculator cal = ctx.getBean("calculator", RecCalculator.class);
         long fiveFact = cal.factorial(5);
         System.out.println("cal.factorial(5) = " + fiveFact);
         System.out.println(cal.getClass().getName());

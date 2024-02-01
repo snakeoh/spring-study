a99235f2e1d11e0b3fbb558895ecce7951742cd0
snakeoh <snakeoh@hanmail.net>
2024-02-01 18:39:48 +0900

    chap 11 - 4. 요청 파라미터 접근

--- spring5fs/sp5-chap11/src/main/java/controller/RegisterController.java
+++ spring5fs/sp5-chap11/src/main/java/controller/RegisterController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,7 +11,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class RegisterController {
 
     @RequestMapping("/register/step1")
-    public String handleStrp1() {
+    public String handleStep1() {
         return "/register/step1";
+    }
+
+    @PostMapping("/register/step2")
+    public String handleStep2(
+        @RequestParam(value = "agree", defaultValue = "false") Boolean agree
+    ) {
+        if (!agree) {
+            return "/register/step1";
+        }
+        return "/register/step2";
     }
 }

f548cb0a2eda46cb905e76b649f303a7efd41eed
snakeoh <snakeoh@hanmail.net>
2024-02-02 16:16:26 +0900

    chap 11 - 6. 커맨드 객체를 이용해서 요청 파라미터 사용하기

--- spring5fs/sp5-chap11/src/main/java/controller/RegisterController.java
+++ spring5fs/sp5-chap11/src/main/java/controller/RegisterController.java
@@ -7,9 +7,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import spring.DuplicateMemberException;
+import spring.MemberRegisterService;
+import spring.RegisterRequest;
 
 @Controller
 public class RegisterController {
+
+    private MemberRegisterService memberRegisterService;
+
+    public void setMemberRegisterService(MemberRegisterService memberRegisterService) {
+        this.memberRegisterService = memberRegisterService;
+    }
 
     @RequestMapping("/register/step1")
     public String handleStep1() {
@@ -29,5 +38,15 @@ public class RegisterController {
     @GetMapping("/register/step2")
     public String handleStep2Get() {
         return "redirect:/register/step1";
+    }
+
+    @PostMapping("/register/step3")
+    public String handleStep3(RegisterRequest regReq) {
+        try {
+            memberRegisterService.regist(regReq);
+            return "/register/step3";
+        } catch (DuplicateMemberException ex) {
+            return "/register/step2";
+        }
     }
 }

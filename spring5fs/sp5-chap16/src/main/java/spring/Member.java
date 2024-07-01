a5f4a87564a26d06989f573559988e7a0e757e0b
snakeoh <snakeoh@hanmail.net>
2024-07-01 16:11:02 +0900

    chap 16 - 3.2 날짜 형식 변환 처리: @JsonFormat 사용

--- spring5fs/sp5-chap16/src/main/java/spring/Member.java
+++ spring5fs/sp5-chap16/src/main/java/spring/Member.java
@@ -2,7 +2,9 @@ package spring;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 
 public class Member {
 
@@ -11,6 +13,7 @@ public class Member {
     @JsonIgnore
     private String password;
     private String name;
+    @JsonFormat(shape = Shape.STRING)
     private LocalDateTime registerDateTime;
 
     public Member(String email, String password, String name, LocalDateTime registerDateTime) {

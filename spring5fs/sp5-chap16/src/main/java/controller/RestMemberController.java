f8ed6d56821ce05c43929fbcbcd1dfdb12f69218
snakeoh <snakeoh@hanmail.net>
2024-07-02 10:01:39 +0900

    chap 16 - 4. @ResquestBody로 JSON 요청 처리

--- spring5fs/sp5-chap16/src/main/java/controller/RestMemberController.java
+++ spring5fs/sp5-chap16/src/main/java/controller/RestMemberController.java
@@ -5,14 +5,20 @@ import java.util.List;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import spring.DuplicateMemberException;
 import spring.Member;
 import spring.MemberDao;
 import spring.MemberRegisterService;
+import spring.RegisterRequest;
+
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 public class RestMemberController {
@@ -23,6 +29,19 @@ public class RestMemberController {
     @GetMapping("/api/members")
     public List<Member> members() {
         return memberDao.selectAll();
+    }
+
+    @PostMapping("/api/members")
+    public void newMember(
+            @RequestBody @Valid RegisterRequest regReq,
+            HttpServletResponse response) throws IOException {
+        try {
+            Long newMemberId = registerService.regist(regReq);
+            response.setHeader("Location", "/api/members/" + newMemberId);
+            response.setStatus(HttpServletResponse.SC_CREATED);
+        } catch (DuplicateMemberException dupEx) {
+            response.sendError(HttpServletResponse.SC_CONFLICT);
+        }
     }
 
     @GetMapping("/api/members/{id}")

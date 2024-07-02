70ecd1ae12b53918a821968b0d917c4f8872456c
snakeoh <snakeoh@hanmail.net>
2024-07-02 10:54:14 +0900

    chap 16 - 5.1 ResponseEntity를 이용한 응답 데이터 처리

--- spring5fs/sp5-chap16/src/main/java/controller/RestMemberController.java
+++ spring5fs/sp5-chap16/src/main/java/controller/RestMemberController.java
@@ -1,12 +1,15 @@
 package controller;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,27 +34,48 @@ public class RestMemberController {
         return memberDao.selectAll();
     }
 
+    // @PostMapping("/api/members")
+    // public void newMember(
+    // @RequestBody @Valid RegisterRequest regReq,
+    // HttpServletResponse response) throws IOException {
+    // try {
+    // Long newMemberId = registerService.regist(regReq);
+    // response.setHeader("Location", "/api/members/" + newMemberId);
+    // response.setStatus(HttpServletResponse.SC_CREATED);
+    // } catch (DuplicateMemberException dupEx) {
+    // response.sendError(HttpServletResponse.SC_CONFLICT);
+    // }
+    // }
+
     @PostMapping("/api/members")
-    public void newMember(
-            @RequestBody @Valid RegisterRequest regReq,
-            HttpServletResponse response) throws IOException {
+    public ResponseEntity<Object> newMember(
+            @RequestBody @Valid RegisterRequest regReq) {
         try {
             Long newMemberId = registerService.regist(regReq);
-            response.setHeader("Location", "/api/members/" + newMemberId);
-            response.setStatus(HttpServletResponse.SC_CREATED);
+            URI uri = URI.create("/api/members/" + newMemberId);
+            return ResponseEntity.created(uri).build();
         } catch (DuplicateMemberException dupEx) {
-            response.sendError(HttpServletResponse.SC_CONFLICT);
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }
     }
 
+    // @GetMapping("/api/members/{id}")
+    // public Member member(@PathVariable Long id,
+    // HttpServletResponse response) throws IOException {
+    // Member member = memberDao.selectById(id);
+    // if (member == null) {
+    // response.sendError(HttpServletResponse.SC_NOT_FOUND);
+    // }
+    // return member;
+    // }
     @GetMapping("/api/members/{id}")
-    public Member member(@PathVariable Long id,
-            HttpServletResponse response) throws IOException {
+    public ResponseEntity<Object> member(@PathVariable Long id) {
         Member member = memberDao.selectById(id);
         if (member == null) {
-            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ErrorResponse("no member"));
         }
-        return member;
+        return ResponseEntity.status(HttpStatus.OK).body(member);
     }
 
     public void setMemberDao(MemberDao memberDao) {
